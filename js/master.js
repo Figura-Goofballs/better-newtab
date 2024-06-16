cb3f9bc7c4d47c9a490c496bf317a1e777867e68
TheBunnyMan123 <bunny@thekillerbunny.com>
2024-06-15 22:43:31 -0500

    font and example dialog layout

--- js/master.js
+++ js/master.js
@@ -10,12 +10,25 @@ window.onload = function() {
     }
 
     document.getElementById("add").onclick = function() {
-        document.getElementById("add-close-container").appendChild(closeButton) ;
+        document.getElementById("add-close-container").appendChild(closeButton);
 
         dialogContainer.appendChild(document.getElementById("add-dialog"));
 
         closeButton.style = 'position: absolute; top: 0; right: 0; translate: translateX(-100%);'
         
         dialog = document.getElementById("add-dialog");
+    }
+
+    document.getElementById("add-panel").onclick = function() {
+        var url = document.getElementById("url-input").value;
+        var name = document.getElementById("name-input").value;
+        var icon = document.getElementById("icon-input").value;
+
+        if (name === '') {
+            name = url
+        }
+
+        console.log(url, name, icon)
+        closeButton.onclick()
     }
 }
